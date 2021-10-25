1df6b6e9176040fa2ea7d0cd6be4159549fd1636
Sean Zhang <sean7218@gmail.com>
2021-10-25 12:47:25 -0400

    Initial Commit

---
+++ nyc-school/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nyc-school
+//
+//  Created by Sean Zhang on 10/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
